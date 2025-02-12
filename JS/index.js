3d69ce5e13038ecaa23d80a173e7c9681401d0de
Iris Mejia <179559543+Esme48@users.noreply.github.com>
2025-02-11 19:06:04 -0700

    Modifications made to the js structure

--- JS/index.js
+++ JS/index.js
@@ -1,43 +1,47 @@
 
 ///////////////////////////////////////////////////////////////////
 function fetchArtworks(){
-  //Fetch Component, a promise that has our data//
+      //Fetch Component, a promise that has our data//
   const projectSection = document.getElementById("gallery");
-//console.log("projectSection:", projectSection);
+      //console.log("projectSection:", projectSection);
   const projectList = projectSection.querySelector("ul");
-  //console.log("projectList", projectList);
+    //console.log("projectList", projectList);
 
 
   fetch("https://api.artic.edu/api/v1/artworks")
-    .then((response)=>{
+  .then((response)=>{
     //Body of the callback function
     return response.json();
   })
-    .then((json) => json.data)
+  .then((json) => json.data)
 
 
-    .then((artworks)=>{
-      console.log("Pieces of Art: ", artworks);
-      for (let i=0; i < artworks.length; i++){
+  .then((artworks)=>{
+    console.log("Pieces of Art: ", artworks);
+    for (let i=0; i < artworks.length; i++){
       const project = artworks[i].title;
       const identifier=artworks[i].image_id;
-      const imgURL=`https://www.artic.edu/iiif/2/${identifier}/full/843,/0/default.jpg`
-      const li= document.createElement("li");
-      const img=document.createElement("img");
-      img.src=imgURL;
-      // img.width=200;//This needs to be in the css, append to new element after the image
-      //innertext of the li element
-      li.innerText = project; //Switch line 30 and 31, create whole new element, the p element in the li
-      li.appendChild(img);
-      // li.innerText = project; //Switch line 30 and 31, create whole new element, the p element in the li
-      projectList.appendChild(li);
-  }
- })
+      console.log(artworks[i]);
+      if(identifier!==null){
+        const imgURL=`https://www.artic.edu/iiif/2/${identifier}/full/843,/0/default.jpg`
+        const li= document.createElement("li");
+        const img=document.createElement("img");
+        img.src=imgURL;
+        // img.width=200;//This needs to be in the css, append to new element after the image
+        //innertext of the li element
+        li.innerText = project; //Switch line 30 and 31, create whole new element, the p element in the li
+        li.appendChild(img);
+        // li.innerText = project; //Switch line 30 and 31, create whole new element, the p element in the li
+        projectList.appendChild(li);
+      }
+    }
+  })
 
 
-      .catch((error)=>{
-      console.log(error);
- });
+
+  .catch((error)=>{
+    console.log(error);
+  });
 }
 
 
@@ -45,66 +49,67 @@ function fetchArtworks(){
 
 
 function fetchArtist(){
-  //Fetch Component, a promise that has our data//
+      //Fetch Component, a promise that has our data//
   const artistSection = document.getElementById("profiles");
-  //console.log("projectSection:", projectSection);
+      //console.log("projectSection:", projectSection);
   const artistList = artistSection.querySelector("ul");
-  //console.log("projectList", projectList);
+      //console.log("projectList", projectList);
 
 
   fetch("https://api.artic.edu/api/v1/agents")
-    .then((response)=>{
+  .then((response)=>{
     //Body of the callback function
     return response.json();
   })
-    .then((json) => json.data)
+  .then((json) => json.data)
 
 
-    .then((agents)=>{
-      console.log("Our Creators: ", agents);
-      for (let i=0; i < agents.length; i++){
-      const artistName = agents[i].title;
-     // const agentID=agents[i].id;
-     // const agentURL=`https://www.artic.edu/iiif/2/${agentID}/full/200,/0/default.jpg`;
-      const li = document.createElement("li");
-     // const img = document.createElement("img");
-      //img.src=agentURL;
-      //innertext of the li element
-      li.innerText = artistName;
-      //li.appendChild(img);
-      artistList.appendChild(li);
-     }
- })
+  .then((agents)=>{
+    console.log("Our Creators: ", agents);
+    for (let i=0; i < agents.length; i++){
+        const artistName = agents[i].title;
+        // const agentID=agents[i].id;
+        // const agentURL=`https://www.artic.edu/iiif/2/${agentID}/full/200,/0/default.jpg`;
+       const li = document.createElement("li");
+        // const img = document.createElement("img");
+        //img.src=agentURL;
+        //innertext of the li element
+        li.innerText = artistName;
+        //li.appendChild(img);
+        artistList.appendChild(li);
+    }
+  })
 
 
-      .catch((error)=>{
-        console.log(error);
-    });
-  }
+  .catch((error)=>{
+   console.log(error);
+  });
+}
 ////////////////////////////////////////////////////////////////
 function fetchExhibits(){
-    //Fetch Component, a promise that has our data//
-    const exhibitSection = document.getElementById("outreach");
-  //console.log("projectSection:", projectSection);
-    const exhibitList = exhibitSection.querySelector("ul");
+      //Fetch Component, a promise that has our data//
+  const exhibitSection = document.getElementById("outreach");
+      //console.log("projectSection:", projectSection);
+  const exhibitList = exhibitSection.querySelector("ul");
     //console.log("projectList", projectList);
   
   
-    fetch("https://api.artic.edu/api/v1/exhibitions")
-      .then((response)=>{
-      //Body of the callback function
-      return response.json();
-    })
-      .then((json) => json.data)
+  fetch("https://api.artic.edu/api/v1/exhibitions")
+  .then((response)=>{
+    //Body of the callback function
+    return response.json();
+  })
+  .then((json) => json.data)
   
   
-      .then((exhibitions)=>{
-        console.log("Our Exhibits: ", exhibitions);
-        for (let i=0; i < exhibitions.length; i++){
-        const exhibitName = exhibitions[i].title;
-        //const identifier=exhibitions[i].id;
-        const imgURL = exhibitions[i].image_url;
-        const li = document.createElement("li");
+  .then((exhibitions)=>{
+   console.log("Our Exhibits: ", exhibitions);
+    for (let i=0; i < exhibitions.length; i++){
+      const exhibitName = exhibitions[i].title;
+      //const identifier=exhibitions[i].id;
+      const imgURL = exhibitions[i].image_url;
+      if(imgURL!==null){
+       const li = document.createElement("li");
         const img=document.createElement("img");
         img.src=imgURL; 
         // img.width=200;
@@ -112,18 +117,19 @@ function fetchExhibits(){
         li.innerText = exhibitName;
         li.appendChild(img);
         exhibitList.appendChild(li);
+     }
     }
-   })
+  }) 
 
   
   
-        .catch((error)=>{
-        console.log(error);
-   });
-  }
+  .catch((error)=>{
+    console.log(error);
+  });
+}
 
   document.addEventListener('DOMContentLoaded', () => {
-   fetchArtist(); 
+    fetchArtist(); 
     fetchArtworks(); 
     fetchExhibits();
 });
@@ -143,3 +149,4 @@ body.appendChild(footer)
 const copyright=document.createElement("p")
 copyright.innerHTML=` © I.Mejia ${thisYear} `
 footer.appendChild(copyright)
+
